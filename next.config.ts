ba9d6fb80ccdd420d603487177ec4e2b70aa6c03
seang541-rgb <seang541@gmail.com>
2026-06-12 13:28:56 +0800

    chore: allow 127 dev origin

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ["127.0.0.1"],
+};
 
 export default withNextIntl(nextConfig);
